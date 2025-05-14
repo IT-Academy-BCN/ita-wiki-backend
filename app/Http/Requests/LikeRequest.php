4e3a36108c4a20771b99ef3bb1b5f2df3ce18fb2
Jordi <jmdesarrollo82@gmail.com>
2025-05-14 12:44:35 +0200

    fix: prepareforvalidation() method is not managing route parameters correctly

--- app/Http/Requests/LikeRequest.php
+++ app/Http/Requests/LikeRequest.php
@@ -25,11 +25,13 @@ class LikeRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    
     protected function prepareForValidation()
     {
-        if ($this->route('github_id')) {
-            $this->merge(['github_id' => $this->route('github_id')]);
+        $githubId = $this->route('github_id');
+        if ($githubId !== null) {
+            $this->merge(['github_id' => $githubId]);
+        } elseif (!$this->has('github_id')) {
+            $this->merge(['github_id' => null]);
         }
     }
 
