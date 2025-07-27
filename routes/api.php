637f4872c82700b89fff628f7353b0fb0884f733
Cristina Soy <ccardonaaltes@hotmail.com>
2025-07-27 12:42:58 +0200

    Spatie installed. Table Roles in conflict  with existing one

--- routes/api.php
+++ routes/api.php
@@ -13,33 +13,59 @@ use App\Http\Controllers\Api\RoleNodeController;
 use App\Http\Controllers\ResourceEditController;
 use App\Http\Controllers\Api\BookmarkNodeController;
 
+
+
+//GitHub Auth Systen Endpoints
 Route::get('/auth/github/redirect', [GitHubAuthController::class, 'redirect']);
 Route::get('/auth/github/callback', [GitHubAuthController::class, 'callback']);
-Route::post('/resources', [ResourceController::class, 'store'])->name('resources.store');
-Route::post('/v2/resources', [ResourceController::class, 'storeResource'])->name('resources.store.v2');
-Route::get('/resources', [ResourceController::class, 'index'])->name('resources');
-Route::get('v2/resources', [ResourceController::class, 'showResource'])->name('showResource');
+
+
+// (Old)Roles Endpoints: in the current permission logic, Roles table refers to Users
 Route::post('/login', [RoleController::class, 'getRoleByGithubId'])->name('login');
-Route::put('/resources/{resource}', [ResourceEditController::class, 'update'])->name('resources.update');
-Route::get('/bookmarks/{github_id}', [BookmarkController::class,'getStudentBookmarks'])->name('bookmarks');
-Route::post('/bookmarks', [BookmarkController::class,'createStudentBookmark'])->name('bookmark.create');
-Route::delete('/bookmarks', [BookmarkController::class,'deleteStudentBookmark'])->name('bookmark.delete');
 Route::post('/roles', [RoleController::class, 'createRole'])->name('roles.create');
 Route::put('/roles', [RoleController::class, 'updateRole'])->name('roles.update');
-Route::get('/likes/{github_id}', [LikeController::class,'getStudentLikes'])->name('likes');
-Route::post('/likes', [LikeController::class,'createStudentLike'])->name('like.create');
-Route::delete('/likes', [LikeController::class,'deleteStudentLike'])->name('like.delete');
-Route::get('/tags', [TagController::class, 'index'])->name('tags');
-Route::get('/tags/frequency', [TagController::class, 'getTagsFrequency'])->name('tags.frequency');
-Route::get('/tags/category-frequency', [TagController::class, 'getCategoryTagsFrequency'])->name('category.tags.frequency');
-Route::get('/tags/by-category', [TagController::class, 'getCategoryTagsId'])->name('category.tags.id');
-
-// FEATURE FLAGS ENDPOINTS
 Route::put('/feature-flags/role-self-assignment', [RoleController::class, 'roleSelfAssignment'])->name('feature-flags.role-self-assignment');
 
 
+//TECHNICAL TESTS ENDPOINTS
+Route::get('/technical-tests', [TechnicalTestController::class, 'index'])->name('technical-tests.index');
+Route::post('/technical-tests', [TechnicalTestController::class, 'store']);
 
-//new github_id to node_id transition ENDPOINTS
+
+//Resources Endpoints
+Route::post('/resources', [ResourceController::class, 'store'])->name('resources.store');
+Route::get('/resources', [ResourceController::class, 'index'])->name('resources');
+Route::put('/resources/{resource}', [ResourceEditController::class, 'update'])->name('resources.update');
+//Resources Endpoints v2
+Route::post('/v2/resources', [ResourceController::class, 'storeResource'])->name('resources.store.v2');
+Route::get('v2/resources', [ResourceController::class, 'showResource'])->name('showResource');
+
+
+//Likes Endpoints
+Route::get('/likes/{github_id}', [LikeController::class,'getStudentLikes'])->name('likes');
+Route::post('/likes', [LikeController::class,'createStudentLike'])->name('like.create');
+Route::delete('/likes', [LikeController::class,'deleteStudentLike'])->name('like.delete');
+
+
+//Bookmarks Endpoints
+Route::get('/bookmarks/{github_id}', [BookmarkController::class,'getStudentBookmarks'])->name('bookmarks');
+Route::post('/bookmarks', [BookmarkController::class,'createStudentBookmark'])->name('bookmark.create');
+Route::delete('/bookmarks', [BookmarkController::class,'deleteStudentBookmark'])->name('bookmark.delete');
+
+
+//Tags Endpoints
+Route::get('/tags/frequency', [TagController::class, 'getTagsFrequency'])->name('tags.frequency');
+Route::get('/tags/category-frequency', [TagController::class, 'getCategoryTagsFrequency'])->name('category.tags.frequency');
+Route::get('/tags/by-category', [TagController::class, 'getCategoryTagsId'])->name('category.tags.id');
+Route::get('/tags', [TagController::class, 'index'])->name('tags');
+
+
+
+/*
+* New github_id to node_id transition ENDPOINTS
+*/ 
+
+//Roles Node Endpoints
 Route::post('/roles-node', [RoleNodeController::class, 'createRoleNode'])->name('roles-node.create');
 Route::put('/roles-node',  [RoleNodeController::class, 'updateRoleNode'])->name('roles-node.update');
 Route::post('/login-node', [RoleNodeController::class, 'getRoleByNodeId'])->name('login-node');
@@ -51,7 +77,6 @@ Route::put('/feature-flags/role-self-assignment-node', [RoleNodeController::clas
 Route::post('/bookmarks-node',   [BookmarkNodeController::class, 'createStudentBookmarkNode'])->name('bookmark-node.create');
 Route::delete('/bookmarks-node', [BookmarkNodeController::class, 'deleteStudentBookmarkNode'])->name('bookmark-node.delete');
 Route::get('/bookmarks-node/{node_id}', [BookmarkNodeController::class, 'getStudentBookmarksNode'])->name('bookmarks-node');
-
 
 //TAGSNODE ENDPOINTS
 Route::get('/tags-node', [TagNodeController::class, 'index'])->name('tags-node');
@@ -66,6 +91,3 @@ Route::get('/tags-node/by-category', [TagNodeController::class, 'getCategoryTags
 //RESOURCESNODE ENDPOINTS
 
 
-//TECHNICAL TESTS ENDPOINTS
-Route::get('/technical-tests', [TechnicalTestController::class, 'index'])->name('technical-tests.index');
-Route::post('/technical-tests', [TechnicalTestController::class, 'store']);
