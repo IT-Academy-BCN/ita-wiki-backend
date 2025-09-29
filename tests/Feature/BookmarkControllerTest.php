352ff03c8a02231209b06c4180decf3112f82be1
Alex Pasquero <axelpasquero@gmail.com>
2025-09-29 13:05:49 +0200

    Deleted comment

--- tests/Feature/BookmarkControllerTest.php
+++ tests/Feature/BookmarkControllerTest.php
@@ -57,7 +57,7 @@ class BookmarkControllerTest extends TestCase
     public function testGetBookmarksForUnexistentRoleFails(): void {
         $nonExistentGithubId = 38928374;
         $response = $this->get('api/bookmarks/' . $nonExistentGithubId);
-        $response->assertStatus(200)  // Cambia da 422 a 200, poiché ora restituisce array vuoto
+        $response->assertStatus(200)  
             ->assertJson([]);
     }
 
