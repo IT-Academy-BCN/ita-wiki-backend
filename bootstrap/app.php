b5a35f031e1c3b63d4709b5c0c4cb9e209eda74c
Alex Pasquero <axelpasquero@gmail.com>
2025-10-04 16:58:32 +0200

    Create RoleController.php and register Middleware on boorstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            'check_permission' => \App\Http\Middleware\CheckPermission::class, // AGGIUNGIAMO QUESTO
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
